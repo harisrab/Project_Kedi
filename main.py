8065d238e13e485e3f2f434a681213cc8618dc2b
harisrab <62747193+harisrab@users.noreply.github.com>
2020-04-15 20:50:31 +0500

    Files Uploaded - Initial Commit

---
+++ main.py
@@ -0,0 +1,23 @@
+#Import libraries and datasets
+print ("[+] Support Libraries Import: Pending")
+import numpy as np                                   #Matrix Manipulation Libs
+import h5py                                          #Dataset Manipulator
+import matplotlib.pyplot as plt                      #Plotting Library
+from testCases_v4a import *
+from dnn_utils_v2 import sigmoid, sigmoid_backward   #Activation Function
+from dnn_utils_v2 import relu, relu_backward         #Activation Function
+from Feyla_Brain_Architecture import *
+print ("[+] Support Libraries Import: Successful")
+
+print("\n[+] Importing Neural Architecture: Pending")
+from Feyla_Brain_Architecture import *
+print("[+] Neural Architecture Import: Successful\n")
+from Feyla_Brain_Architecture import *
+
+#Main
+def main():
+    pass
+
+#Intantiate Main
+if __name__ == '__main__':
+    main()
